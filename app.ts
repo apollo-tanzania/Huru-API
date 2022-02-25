150a4e0bece608c3912267e960375d318a7d2387
masakachristopher <masakachristopher@yahoo.com>
2022-02-25 19:01:24 +0300

    configure app logger

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
 import * as dotenv from 'dotenv';
 import express, { Request, Response,Application } from 'express';
-import * as winston from 'winston';
-import * as expressWinston from 'express-winston';
 import cors from 'cors';
+import morganMiddleware from './config/middleware/morgan';
 
 dotenv.config();
 
@@ -15,13 +14,13 @@ const PORT: number = parseInt(process.env.PORT, 10);
 const app : Application = express();
 
 //app configuration
+app.use(morganMiddleware)
 app.use(cors());
 app.use(express.json())
 
 //routes
 app.get('/', (req: Request, res: Response) => {
     res.send('Something is happening!!')
-
     }
 );
 
